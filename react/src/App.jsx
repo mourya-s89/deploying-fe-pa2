cfadd7ca16c6b6506b2ce3cb45b24800d7411792
mourya-s89 <kadali.mourya.s89@kalvium.community>
2025-04-02 14:36:18 +0530

    pa2

---
+++ react/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Hi iam k.mourya</h1>
+      <p>Email: kadali@gmail.com</p>
+      <p>Phone: +91 12345 67890</p>
+      </>
+  );
+}
+
+export default App;
